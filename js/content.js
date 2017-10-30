aad95871c48946c310f02a62654bef8ab9dbc9aa
strongTing <s8527502987@gmail.com>
2017-10-30 18:01:49 +0800

    2017/10/30 18:02 FIX: -the function that hide the sponsor post

--- js/content.js
+++ js/content.js
@@ -2,31 +2,36 @@
 document.getElementById('pagelet_ego_pane').style.display = 'none';
 
 //listen post num add
-var post_num = 0;
+var post_num_changed = 0;
+var post_num_now = 0;
 var check_num = 0;
-$(document).bind('DOMSubtreeModified',function(){
-    var post_numTemp = get_post_num();
-    var check_numTemp = check_post_load();
-    if(post_numTemp > post_num && check_numTemp > check_num){
-        var time_now = new Date();
-        console.log('load post:'+post_numTemp,'time:'+time_now.getTime());
-        post_sponsor(post_num,post_numTemp);
-    }
-    post_num = post_numTemp;
-//    console.log(post_numTemp);
+//to listen content area when the modified have sponsor to hide it!
+$('#contentArea').bind('DOMSubtreeModified',function(){
+    post_num_changed,post_num_now = get_post_num(post_num_changed,post_num_now);
+    post_sponsor(post_num_changed,post_num_now);
 });
 
-function get_post_num(){
-//    var post_condition_0 = $("div[id*='hyperfeed']");  //the condition cant work perfect
-    var post_condition_1 = $("div[class*='_5jmm _5pat']");
-    var post = post_condition_1 ;
+function get_post_num(post_num_changed,post_num_now){
+    var post = $("div[class*='_5jmm _5pat']");
     var post_num = post.length;
-    return post_num;
+    if(post_num>post_num_now)
+    {
+        post_num_changed = post_num_now;
+        post_num_now = post_num;
+    }
+    return post_num_changed,post_num_now;
 }
 
-function check_post_load(){
+function check_post_time_load(){
     var check = $('._5pcq');
-    var check_num = check.length;
+    var check_num = 0;
+    for (var i = check.length -1;i>0;i--)
+    {
+        if(check[i].href != undefined)
+        {
+            check_num++;
+        }
+    }
     return check_num;
 }
 
@@ -34,22 +39,18 @@ function check_post_load(){
 var sponsor_num = 0;
 function post_sponsor(post_num_changed,post_num_now)
 {
-    var post = $("div[class*='_5jmm _5pat']").find(':contains(贊助)');
-    console.log("post_sponsor:" + post.length);
-    if(post.length>sponsor_num)
+    
+    var sponsor_numTemp = $("div[class*='_5jmm _5pat']").find("div[id*='feed_subtitle']").children(':contains(贊助)');
+    if(sponsor_numTemp.length>sponsor_num)
     {
-    for(var i=  post_num_changed;i<post_num_now;i++)
-    {
-        var ad = $("div[class*='_5jmm _5pat']").eq(i).find("div[id*='feed_subtitle']")//.find(':contains(贊助)');
-        console.log(i);
-        console.log($("div[class*='_5jmm _5pat']").eq(i));
-        console.log(ad);
-        if(ad.length > 0)
+        for(var i=  post_num_changed;i<post_num_now;i++)
         {
-            $("div[class*='_5jmm _5pat']").eq(i).css("color","yellow");
-            console.log('changed');
+            var ad = $("div[class*='_5jmm _5pat']").eq(i).find("div[id*='feed_subtitle']").children(':contains(贊助)');
+            if(ad.length > 0)
+            {
+                $("div[class*='_5jmm _5pat']").eq(i).css("color","yellow");
+            }
         }
+    sponsor_num = sponsor_numTemp.length;
     }
-    }
-    sponsor_num = post.length;
 }
