308f7cdb1c179cdb220b626a9d9bb3f6eea2c9b1
strongTing <s8527502987@gmail.com>
2017-11-02 23:45:35 +0800

    2017/11/02 23:45 rebuild content script ADD: -use MutationObserver object to replace DOMSubtreeModified event -get post content

--- js/content.js
+++ js/content.js
@@ -1,85 +1,45 @@
-//hide side ad
-document.getElementById('pagelet_ego_pane').style.display = 'none';
-
-//listen post num add
-var post_num_changed = 0;
-var post_num_now = 0;
-var check_num = 0;
-//to listen content area when the modified have sponsor to hide it!
-$('#contentArea').bind('DOMSubtreeModified',function(){
-    var get_post_start_end = get_post_num(post_num_changed,post_num_now);
-    post_num_changed = get_post_start_end.post_num_changed; 
-    post_num_now = get_post_start_end.post_num_now;
-    post_sponsor(post_num_changed,post_num_now);
-    get_profileAndLink(post_num_changed,post_num_now);
-});
-
-function get_post_num(post_num_changed,post_num_now){
-    var post = $("div[class*='_5jmm _5pat']");
-    var post_num = post.length;
-    if(post_num>post_num_now)
-    {
-        post_num_changed = post_num_now;
-        post_num_now = post_num;
-
+const block_slide = ()=>{
+    try{
+    $("#pagelet_ego_pane").hide();
     }
-    return {
-        post_num_changed:post_num_changed,
-        post_num_now:post_num_now
+    catch(e){
+        console.log('not have the slide ad')
+    }
+}
+
+const dom_modified = ()=>{
+    let jquery = 0;
+    $("#pagelet_ego_pane").on('DOMSubtreeModified',function(){
+        block_slide();
+    });
+    $("#stream_pagelet").on('DOMSubtreeModified',function(){
+        let post = post_content(); 
+        console.log(post.length);
+    });
+}
+
+const dom_modified_2=()=>{
+    const dom_listenser=(idName,mutationHandler)=>{
+        let target = document.querySelector(idName);
+        let config = { attributes: true, childList: true, characterData: true,subtree:true};
+        let observer = new MutationObserver(mutationHandler);
+        observer.observe(target, config);
     };
+
+    dom_listenser('#stream_pagelet',(mutations)=>{
+        let post = post_content();
+        console.log(post.length);
+    });
 }
 
-function check_post_time_load(){
-    var check = $('._5pcq');
-    var check_num = 0;
-    for (var i = check.length -1;i>0;i--)
-    {
-        if(check[i].href != undefined)
-        {
-            check_num++;
-        }
-    }
-    return check_num;
+const post_content = ()=>{
+    let post = $('._5jmm._5pat._3lb4.d_1ox2hq53yy');
+    return post 
 }
 
-//need to notice loading
-var sponsor_num = 0;
-function post_sponsor(post_num_changed,post_num_now)
-{
-    
-    var sponsor_numTemp = $("div[class*='_5jmm _5pat']").find("div[id*='feed_subtitle']").children(':contains(贊助)');
-    if(sponsor_numTemp.length>sponsor_num)
-    {
-        for(var i=  post_num_changed;i<=post_num_now;i++)
-        {
-            var ad = $("div[class*='_5jmm _5pat']").eq(i).find("div[id*='feed_subtitle']").children(':contains(贊助)');
-            if(ad.length > 0)
-            {
-                $("div[class*='_5jmm _5pat']").eq(i).css("color","yellow");
-                console.log('the post of sponsor:' + i);
-            }
-        }
-    sponsor_num = sponsor_numTemp.length;
-    }
+const first_work = ()=>{
+//    dom_modified();
+    dom_modified_2();
+    block_slide();
 }
-
-var profile_num = 0;
-function get_profileAndLink(post_num_changed,post_num_now)
-{
-    var profile_numTemp = $("div[class*='_5jmm _5pat']").find('.fwb.fcg').length;
-    if(profile_numTemp > profile_num)
-    {
-        for(var i=  post_num_changed;i<=post_num_now;i++)
-        {
-            var profile = $("div[class*='_5jmm _5pat']").eq(i).find('.fwb.fcg');
-            if(profile.length>0)
-            {
-                console.log('/////////////////////////////');
-                    console.log('the '+i+' post:'+profile[0].innerText);
-                    console.log('the '+i+' post:'+profile[0].lastChild.href);
-                console.log('/////////////////////////////');
-            }
-        }
-    }
-    profile_num = profile_numTemp;
-}
+first_work();
