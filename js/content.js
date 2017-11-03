dadd03fd70462f429f4fd48e080b7948f5985ff0
strongTing <s8527502987@gmail.com>
2017-11-03 13:19:07 +0800

    2017/11/03 13:20 ADD -post function that can get content,judgment is the post a sponsor ,get post detail include the post href,time, name and href of author

--- js/content.js
+++ js/content.js
@@ -18,6 +18,47 @@ const dom_modified = ()=>{
     });
 }
 
+let post =(start,end)=> {
+    let handle_num = start;
+    const get_content = (handle)=>{
+        let post = $('._5jmm._5pat._3lb4.d_1ox2hq53yy').eq(handle_num);
+        handle(post);
+    }
+
+    const is_sponsor = (post)=>{
+        let sponsor = post.children(':contains(贊助)');
+        if(sponsor.length > 0){
+            post.css("color","yellow");
+            console.log(handle_num+'  is sponsor');
+        }
+        else{
+            get_detail(post);
+        }
+    }
+    
+    const get_detail = (post)=>{
+        let href='',
+            author_name='',
+            author_href='',
+            time='';
+        href = post.find('._5pcq')[0].href;
+        author_name = post.find('.fwb.fcg')[0].innerText;        
+        author_href = post.find('.fwb.fcg')[0].lastChild.href;
+        time = post.find('abbr._5ptz')[0].title;
+        
+        console.log('post_href:'+href);
+        console.log('author_name:'+author_name);
+        console.log('author_href:'+author_href);
+        console.log('time:'+time);
+    }
+
+    for(handle_num;handle_num<=end;handle_num++){
+        get_content(is_sponsor); 
+    }
+    
+}
+post(0,1);
+
 const dom_modified_2=()=>{
     const dom_listenser=(idName,mutationHandler)=>{
         let target = document.querySelector(idName);
@@ -27,15 +68,10 @@ const dom_modified_2=()=>{
     };
 
     dom_listenser('#stream_pagelet',(mutations)=>{
-        let post = post_content();
-        console.log(post.length);
     });
 }
 
-const post_content = ()=>{
-    let post = $('._5jmm._5pat._3lb4.d_1ox2hq53yy');
-    return post 
-}
+
 
 const first_work = ()=>{
 //    dom_modified();
