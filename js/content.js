9d536b2f5a28be64aecebb8365c949fa2a30506a
strongTing <s8527502987@gmail.com>
2017-11-04 19:09:05 +0800

    2017/11/04 19:10 ADD -the funtion to get the loaded and loading post num BUG -some post will get the href and name that not only author but also other people

--- js/content.js
+++ js/content.js
@@ -22,42 +22,93 @@ let post =(start,end)=> {
     let handle_num = start;
     const get_content = (handle)=>{
         let post = $('._5jmm._5pat._3lb4.d_1ox2hq53yy').eq(handle_num);
+        if(post.find('the_post_done').length>0){
+//            console.log('heve done');
+        }
+        else{
         handle(post);
-    }
+        }
+    };
+    //when the post have been handled append a class,in order to check it.
+    const done =(post)=>{
+        let done = document.createElement("the_post_done");
+        let messageNode = document.createTextNode('have done');
+        //pElement.style.display='none'; 
+        done.append(messageNode);
+        post.append(done);
+    };
 
     const is_sponsor = (post)=>{
         let sponsor = post.children(':contains(贊助)');
+
         if(sponsor.length > 0){
             post.css("color","yellow");
-            console.log(handle_num+'  is sponsor');
+            console.log('the num:' +handle_num+'  is sponsor');
+            done(post);
         }
         else{
             get_detail(post);
         }
-    }
-    
+
+            };
     const get_detail = (post)=>{
         let href='',
             author_name='',
             author_href='',
             time='';
-        href = post.find('._5pcq')[0].href;
-        author_name = post.find('.fwb.fcg')[0].innerText;        
-        author_href = post.find('.fwb.fcg')[0].lastChild.href;
-        time = post.find('abbr._5ptz')[0].title;
-        
-        console.log('post_href:'+href);
-        console.log('author_name:'+author_name);
-        console.log('author_href:'+author_href);
-        console.log('time:'+time);
-    }
+
+        try{
+            href = post.find('._5pcq')[0].href;
+            try{
+                let author =  post.find('.fwb');
+                for(let i=author.length-1;i>=0;i--){
+                    author_name = author_name +' , ' +author.eq(i).find('a').text();        
+                    console.log(author.eq(i).find('a'));
+                    author_href =author_href+' , '+author[i].lastChild.href;
+                }
+            }
+            catch(e){
+                console.log(e);
+            }
+            time = post.find('abbr._5ptz')[0].title;
+
+            console.log('the num:'+handle_num);
+            console.log('post_href:'+href);
+            console.log('author_name:'+author_name);
+            console.log('author_href:'+author_href);
+            console.log('time:'+time);
+            console.log('/////////////////////////////');
+
+            done(post);
+        }
+        catch(e){
+        //    console.log(e);
+        }
+
+    };
 
     for(handle_num;handle_num<=end;handle_num++){
         get_content(is_sponsor); 
     }
     
+};
+
+const get_post_num=(last_load,load)=>{
+    let post = $('._5jmm._5pat._3lb4.d_1ox2hq53yy');
+    let post_num = post.length - 1 ;
+    if(post_num>load)
+    {
+        last_load = load;
+        load = post_num;
+
+    }
+    return {
+        last_load:last_load,
+        load:load
+    };
 }
-post(0,1);
+
+
 
 const dom_modified_2=()=>{
     const dom_listenser=(idName,mutationHandler)=>{
@@ -66,8 +117,14 @@ const dom_modified_2=()=>{
         let observer = new MutationObserver(mutationHandler);
         observer.observe(target, config);
     };
-
+    let last_load =0;
+    let load = 0;
     dom_listenser('#stream_pagelet',(mutations)=>{
+        let num = get_post_num(last_load,load);
+        last_load = num.last_load;
+        load = num.load;
+        console.log(last_load,load);
+        post(last_load,load);
     });
 }
 
